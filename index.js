1f2231d207b8e15c5b5c5ece26aa2f8671b5ef05
Vladislav Hristov <vlad@Mac-mini.local>
2018-11-20 15:59:32 +0200

    Add eCollect eslint nodejs rules and change the code accordingly

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'user strict';
+'use strict';
 
 const FPEEncryptor = require('./lib/fpe_encryptor');
 const factor = require('./lib/factor');
@@ -9,7 +9,7 @@ module.exports = {
 	 * Generic Z_n FPE encryption, FE1 scheme
 	 *
 	 * @param {number} modulus - Use to determine the range of the numbers. Example, if the numbers range from 0 to 999, use "1000" here
-	 * @param {number} subject - The number to decrypt. Must be < modulus.
+	 * @param {number} subject - The number to encrypt. Must be < modulus.
 	 * @param {string} key - key Secret key to encrypt with. Must be compatible with HMAC(SHA256).  See https://tools.ietf.org/html/rfc2104#section-3 for recommendations,
 	 * on key length and generation.  Anything over 64 bytes is hashed to a 64 byte value, 32 bytes is generally considered "good enough" for most applications.
 	 * @param {string} tweak - Non-secret parameter, think of it as an initialisation vector. Must be non-null and at least 1 byte long, no upper limit.
